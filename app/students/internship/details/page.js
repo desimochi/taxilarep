9ce0fa0f8fedc2167556a7b34d1c60adc9b4b826
Puneet Sharma <puni199711@gmail.com>
2025-03-24 14:14:46 +0530

    filenamessasdasdaddd

--- app/students/internship/details/page.js
+++ app/students/internship/details/page.js
@@ -9,28 +9,10 @@ export default function AnswerDetails() {
   const [data, setData] = useState(null);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false)
-  useEffect(() => {
-        const fetchClassData = async () => {
-            try {
-                setLoading(true)
-                const response = await authFetch(`student-${subcomp?'subcomponent':'component'}-answer-details/${id}/${compId}`)
-                if (!response.ok) throw new Error("Failed to fetch Subject data")
 
-                const data = await response.json()
-                if (data.data) setData(data.data) // ✅ No TypeError here
-            } catch (err) {
-                setError(err.message)
-            } finally {
-                setLoading(false)
-            }
-        }
-
-        fetchClassData()
-    }, [id, compId, subcomp])
 
   return (
     <div>
-      <h1>Answer Details - {id} {compId} {data?.answers_file}</h1>
-    </div>
+      Internship    </div>
   );
 }
