905de67393f55e64550ee2e8f8bc2ed2287b22a5
desimochi <desimochi0202@gmail.com>
2025-08-28 13:11:56 +0530

    api created and change some error

--- app/game/rat-race/page.js
+++ app/game/rat-race/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { GlobalContext } from "@/components/GlobalContext";
+import { Award, HelpCircle } from "lucide-react";
 import Link from "next/link";
 import { useContext } from "react";
 
@@ -10,8 +11,11 @@ export default function HtmlPage() {
       const encodedStateName = encodeURIComponent(JSON.stringify(state.name));
      console.log(encodedState)
   return (
-    <div className="w-full h-screen bg-gray-100">
-    
+    <div className="w-full h-screen bg-black py-8">
+     <div class="max-w-7xl mx-auto flex justify-between mb-3">
+            <Link href="/game/rat-race/how-to-play" class="bg-yellow-100 text-yellow-700 px-4 py-1 rounded-sm flex items-center gap-1"><HelpCircle /> Game Instructions</Link>
+            <Link href="/game/rat-race/leaderboard" class="bg-green-100 text-green-700 px-4 py-1 rounded-sm flex items-center gap-1"> <Award /> See LeaderBoard</Link>
+        </div>
       <iframe
         src={`/rat-race.html?userId=${encodedState}&name=${encodedStateName}`}
         className="w-full h-full border-none"
