b4170519d2e4e1e05e2010399c0335ade928634b
desimochi <desimochi0202@gmail.com>
2025-09-04 16:00:18 +0530

    Sector Shaker Game change

--- app/game/sector-shaker/page.js
+++ app/game/sector-shaker/page.js
@@ -17,7 +17,7 @@ export default function HtmlPage() {
             <Link href="/game/sector-shaker/leaderboard" className="bg-green-100 text-green-700 px-4 py-1 rounded-sm flex items-center gap-1"> <Award /> See LeaderBoard</Link>
         </div>
       <iframe
-        src={`/sectorShaker2.html?userId=${encodedState}&name=${encodedStateName}`}
+        src={`/sectorshaker2.html?userId=${encodedState}&name=${encodedStateName}`}
         className="w-full h-full border-none"
       />
     </div>
