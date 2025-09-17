0be2c38d8d36eac1a2484ebfaaf060b386201886
desimochi <desimochi0202@gmail.com>
2025-09-17 13:14:07 +0530

    ai videp

--- app/game/interview-preparation/page.js
+++ app/game/interview-preparation/page.js
@@ -19,7 +19,7 @@ export default function HtmlPage() {
             <Link href="/game/interview-preparation/leaderboard" class="group bg-green-100 text-green-700 mt-4 flex items-center gap-2 justify-center px-8 py-3 mb-3 cursor-pointer rounded-md text-sm transition-all duration-300 disabled:opacity-50 disabled:cursor-not-allowed"> <Award className="h-0 w-0 opacity-0 transform translate-x-[-5px] transition-all duration-300 group-hover:opacity-100 group-hover:w-4 group-hover:h-4 group-hover:translate-x-0" /> See LeaderBoard</Link>
         </div>
       <iframe
-        src={`/mock.html?userId=${encodedState}&name=${encodedStateName}&apiKey=${apiKey}`}
+        src={`/Videoint.html?userId=${encodedState}&name=${encodedStateName}&apiKey=${apiKey}`}
         className="w-full h-full border-none"
       />
     </div>
