b382c9aa4d1d1728aff27dfff0f6bdf6697aa39b
desimochi <desimochi0202@gmail.com>
2025-11-27 16:25:52 +0530

    games name chnage

--- app/game/diligence/page.js
+++ app/game/diligence/page.js
@@ -12,7 +12,7 @@ export default function HtmlPage() {
      console.log(encodedState)
   return (
     <div className="w-full h-screen bg-gray-900 py-8">
-        <h1 className="text-center text-white font-bold text-4xl mb-4">Airhniti</h1>
+        <h1 className="text-center text-white font-bold text-4xl mb-4">Diligence</h1>
      <div className="max-w-7xl mx-auto flex justify-between mb-3">
             <Link href="/game/diligence/how-to-play" className="bg-yellow-100 text-yellow-700 px-4 py-1 rounded-sm flex items-center gap-1"><HelpCircle /> Game Instructions</Link>
             <Link href="/game/diligence/leaderboard" className="bg-green-100 text-green-700 px-4 py-1 rounded-sm flex items-center gap-1"> <Award /> See LeaderBoard</Link>
