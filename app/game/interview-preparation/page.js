90a5bcd6b08cf59f893cc23c95d5f83bfc8fa598
desimochi <desimochi0202@gmail.com>
2025-12-01 15:53:16 +0530

    a[i cjanhe

--- app/game/interview-preparation/page.js
+++ app/game/interview-preparation/page.js
@@ -10,7 +10,7 @@ export default function HtmlPage() {
      const {state} =  useContext(GlobalContext)
      const encodedState = encodeURIComponent(JSON.stringify(state.id));
       const encodedStateName = encodeURIComponent(JSON.stringify(state.name));
-      const apiKey = "AIzaSyBMd8cwKx0rvLuhqSic4iwGiDCX7-X4Kh8";
+      const apiKey = "AIzaSyAUUaeq5tRk3RrAYgHQDSuiHZDfu6Zzd_0";
      console.log(encodedState)
   return (
     <div className="w-full h-screen py-8 bg-white px-8">
