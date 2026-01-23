e005e48790bb62c8fdfcf9db41fbd34b754cd65d
desimochi <desimochi0202@gmail.com>
2026-01-23 11:29:35 +0530

    aps

--- app/ClientLayout.js
+++ app/ClientLayout.js
@@ -29,7 +29,6 @@ function Layout({ children }) {
     const isNoLayoutRoute = noLayoutRoutes.includes(pathname);
     const userCookie = Cookies.get("user");
     const token = localStorage.getItem("accessToken");
-console.log("Checking authentication for route:", userCookie, token, pathname);
     // If it's a no-layout route, just render the children without a check
     if (isNoLayoutRoute) {
       setShowLayout(false); // Indicate that the main layout should not be shown
